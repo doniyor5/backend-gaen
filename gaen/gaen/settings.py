eaf40a9d5bf4499c4d9aad40e02b61e287603bc7
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2024-11-01 08:04:50 +0500

    Update settings.py

--- gaen/gaen/settings.py
+++ gaen/gaen/settings.py
@@ -172,19 +172,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-if DEBUG:
-    MEDIA_URL = 'media/'
-    MEDIA_ROOT = BASE_DIR / 'media'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
 
-else:
-    MEDIA_URL = 'mediafiles/'
-    MEDIA_ROOT = BASE_DIR / 'mediafiles'
+STATIC_URL = '/static/'
 
-STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
-STATIC_ROOT = '/var/www/gaen-static/'
-
+STATIC_ROOT = os.getenv('STATIC_ROOT', default=BASE_DIR / 'staticfiles')
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
